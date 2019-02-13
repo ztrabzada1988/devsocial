3b6164e59bb72a3681eeb890eed219bb54109da1
Zubair Trabzada <ztrabzada@losaltosca.gov>
2019-02-13 15:52:59 -0800

    User and users.js updated to include data from mongoDB

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,40 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+
+// Load user model
+const User = require('../../models/User');
 
 // @route   GET api/users/test
 // @desc    Tests users route
 // @access  Public
-
 router.get('/test', (req, res) => { // on the browser it will be api/users/test not /test
     res.json({ msg: "Users Works"});
+});
+
+// @route   GET api/users/register
+// @desc    Register user
+// @access  Public
+router.post('/register', (req, res) => {
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if(user) {
+                return res.status(400).json({ email: 'Email already exists' });
+            } else {
+                const avatar = gravatar.url(req.body.email, { // see garavatr docs for below
+                    s: '200', // Size
+                    r: 'pg', // Rating
+                    d: 'mm' // Default
+                })
+
+                const newUser = new User({ // below will be coming in from our frontend app (react form)
+                    name: req.body.name,
+                    email: req.body.email,
+                    avatar,
+                    password: req.body.password
+                })
+            }
+        })
 });
 
 module.exports = router;
