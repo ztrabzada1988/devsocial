47adbd0bbd0a83601169b240a343616169e2f6b1
Zubair Trabzada <ztrabzada@losaltosca.gov>
2019-02-13 17:11:51 -0800

    register users and login functionality added

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 
 // Load user model
 const User = require('../../models/User');
@@ -25,16 +26,54 @@ router.post('/register', (req, res) => {
                     s: '200', // Size
                     r: 'pg', // Rating
                     d: 'mm' // Default
-                })
+                });
 
                 const newUser = new User({ // below will be coming in from our frontend app (react form)
                     name: req.body.name,
                     email: req.body.email,
                     avatar,
                     password: req.body.password
-                })
+                });
+                // hash the password
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if(err) throw err;
+                        newUser.password = hash; // set the user's password to the hashed password
+                        newUser.save()
+                            .then(user => res.json(user))
+                            .catch(err => console.log(err));
+                    });
+                });
             }
-        })
+        });
 });
+
+
+// @route   GET api/users/login
+// @desc    Login User / Returning JWT Token
+// @access  Public
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // Find user by email
+    User.findOne({ email })
+        .then(user => {
+            //Check for user
+            if(!user) {
+                return res.status(404).json({ email: 'User not found'});
+            }
+
+            // Check Password
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if(isMatch) {
+                        res.json({msg: "Success"});
+                    } else {
+                        return res.status(400).json({password: 'Password incorrect'})
+                    }
+                });
+        })
+})
 
 module.exports = router;
