594077bfcf29ef5a5d8a1a8758e9bc963f97389d
Zubair Trabzada <zubair.trabzada@gmail.com>
2019-02-28 23:40:07 -0800

    adding auth

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -8,6 +8,14 @@ const router = express.Router();
 router.get("/test", (req, res) => {
   // on the browser it will be api/profile/test not /test
   res.json({ msg: "Profile Works" });
+  passport.authenticate("jwt", { session: false }),
+    (req, res) => {
+      res.json({
+        id: req.user.id,
+        name: req.user.name,
+        email: req.user.email
+      });
+    };
 });
 
 module.exports = router;
