557204daa8316858075d72f77e3c42f6df0f6090
Zubair Trabzada <zubair.trabzada@gmail.com>
2019-03-13 22:53:19 -0700

    update posts

--- models/Posts.js
+++ models/Posts.js
@@ -6,39 +6,5 @@ const PostsSchema = new Schema({
   post: {
     type: String,
     required: true
-  },
-  name: {
-    type: String,
-    required: true
-  },
-
-  firstpost: [
-    {
-      title: {
-        type: String,
-        required: true
-      },
-      post: {
-        type: String,
-        required: true
-      },
-      location: {
-        type: String
-      },
-      from: {
-        type: Date,
-        required: true
-      },
-      to: {
-        type: Date
-      },
-      current: {
-        type: Boolean,
-        default: false
-      },
-      description: {
-        type: String
-      }
-    }
-  ]
+  }
 });
