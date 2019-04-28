5159461e9420f1db9f87abd7790072b799362ae4
Zubair Trabzada <zubair.trabzada@gmail.com>
2019-04-27 23:58:38 -0700

    testing routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -129,20 +129,4 @@ router.get(
   }
 );
 
-//TEST
-// @route   GET api/users/current
-// @desc    Return current user
-// @access  Private
-router.get(
-  "/test",
-  passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    res.json({
-      id: req.user.id,
-      name: req.user.name,
-      email: req.user.email
-    });
-  }
-);
-
 module.exports = router;
